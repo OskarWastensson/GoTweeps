08b5f6ab59773f3b377306b2eabebf2ba05c0812
Hector Romo <hectitor.romo@gmail.com>
2012-02-21 13:16:59 +0100

    Två markers i index

--- frontend/script.js
+++ frontend/script.js
@@ -6,14 +6,9 @@
 
 var geocoder;
 var map;
-var departure;
-var destination;
+var marker;
 var initialLocation;
 var browserSupportFlag = new Boolean();
-var directionsDisplay = new google.maps.DirectionsRenderer();
-var depLocation;
-var destLocation;   
-   
     
 function initialize(){
 //MAP
@@ -24,18 +19,15 @@ function initialize(){
     center: sthlm,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-    
         
   map = new google.maps.Map(document.getElementById("map_canvas"), options);
-  directionsDisplay.setMap(map);
-
+  
   //W3C geolocation
   if(navigator.geolocation){
     browserSupportFlag = true;
     navigator.geolocation.getCurrentPosition(function(position){
       initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
       map.setCenter(initialLocation);
-      console.log("BRAA1");
     }, function(){
       handleNoGeolocation(browserSupportFlag);
     });
@@ -69,59 +61,144 @@ function initialize(){
     
   //GEOCODER
   geocoder = new google.maps.Geocoder();
-  departure = new google.maps.Marker({
+
+  marker = new google.maps.Marker({
     map: map,
     position: initialLocation,
     draggable: true
   });
 
-  destination = new google.maps.Marker({
-    map: map,
-    draggable: true
-  });
-          console.log(destination);
-
         
 }
-  
-function directions(destination, origin, locations) {
-    var directionsService = new google.maps.DirectionsService();
-
-    var waypoints = [];
-    $.each(locations, function(i, location) {
-      waypoints.push({location: location, stopover: true});
-    }); 
-
-    var request = {
-        origin: origin ,
-        destination: destination,
-        waypoints: waypoints, 
-        travelMode: google.maps.DirectionsTravelMode.DRIVING,
-        optimizeWaypoints: true,
-    };
-    console.log('DirectionsService:');
-    directionsService.route(request, function(response, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(response);
-        var data = update_legs(response.routes[0], response.cg.destination);
-        console.log(response);
-      } else {
-        // bad request to directions!
-        console.log(status);
-      }
-
-    });
-    return data;
-  }
-  
-  
     
+
+
+
 $(document).ready(function() { 
          
   initialize();
           
   $(function() {
-    $(".address").autocomplete({
+     
+      $("#address1").autocomplete({
+       
+      //This bit uses the geocoder to fetch address values
+      source: function(request, response) {
+        geocoder.geocode( {'address': request.term }, function(results, status) {
+          response($.map(results, function(item) {
+            return {
+              label:  item.formatted_address,
+              value: item.formatted_address,
+              latitude: item.geometry.location.lat(),
+              longitude: item.geometry.location.lng()
+            }
+          }));
+        })
+       
+
+      },
+      //This bit is executed upon selection of an address
+      select: function(event, ui) {
+        $("#latitude1").val(ui.item.latitude);
+        $("#longitude1").val(ui.item.longitude);
+<<<<<<< HEAD
+        depLocation = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
+        if(destLocation) {
+          directions(depLocation, destLocation, []);
+
+        } else {
+          departure.setPosition(depLocation);
+          map.setCenter(depLocation);
+=======
+
+        var location = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
+        marker = new google.maps.Marker({
+          map: map,
+          position: location,
+          draggable: true
+        })
+
+        marker.setPosition(location);
+        map.setCenter(location);
+
+
+      }
+    });
+  });
+  
+  //Add listener to marker for reverse geocoding
+  google.maps.event.addListener(marker, 'drag', function() {
+    geocoder.geocode({'latLng': marker.getPosition()}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          $('#address1').val(results[0].formatted_address);
+          $('#latitude1').val(marker.getPosition().lat());
+          $('#longitude1').val(marker.getPosition().lng());
+>>>>>>> Två markers i index
+        }
+      }
+    });
+  });
+
+
+    $(function() {
+     
+      $("#address2").autocomplete({
+       
+      //This bit uses the geocoder to fetch address values
+      source: function(request, response) {
+        geocoder.geocode( {'address': request.term }, function(results, status) {
+          response($.map(results, function(item) {
+            return {
+              label:  item.formatted_address,
+              value: item.formatted_address,
+              latitude: item.geometry.location.lat(),
+              longitude: item.geometry.location.lng()
+            }
+          }));
+        })
+       
+
+      },
+      //This bit is executed upon selection of an address
+      select: function(event, ui) {
+        $("#latitude2").val(ui.item.latitude);
+        $("#longitude2").val(ui.item.longitude);
+
+        var location = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
+        marker = new google.maps.Marker({
+          map: map,
+          position: location,
+          draggable: true
+        })
+
+        marker.setPosition(location);
+        map.setCenter(location);
+
+
+      }
+    });
+  });
+  
+  //Add listener to marker for reverse geocoding
+  google.maps.event.addListener(marker, 'drag', function() {
+    geocoder.geocode({'latLng': marker.getPosition()}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          $('#address2').val(results[0].formatted_address);
+          $('#latitude2').val(marker.getPosition().lat());
+          $('#longitude2').val(marker.getPosition().lng());
+        }
+      }
+    });
+  });
+
+
+});
+
+/*
+$(function() {
+    $("#address1").autocomplete({
       //This bit uses the geocoder to fetch address values
       source: function(request, response) {
         geocoder.geocode( {'address': request.term }, function(results, status) {
@@ -137,42 +214,28 @@ $(document).ready(function() {
       },
       //This bit is executed upon selection of an address
       select: function(event, ui) {
-        $("#latitude1").val(ui.item.latitude);
-        $("#longitude1").val(ui.item.longitude);
-        depLocation = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
-        if(destLocation) {
-          directions(depLocation, destLocation, []);
-
-        } else {
-          departure.setPosition(depLocation);
-          map.setCenter(depLocation);
-        }
-      },
-      select: function(event, ui) {
-        $("#latitude2").val(ui.item.latitude);
-        $("#longitude2").val(ui.item.longitude);
-        destLocation = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
-        if(depLocation) {
-          directions(depLocation, destLocation, []);
-        };
-        /*destination.setPosition(destLocation);
-        map.setCenter(destLocation);*/
+        $("#latitude").val(ui.item.latitude);
+        $("#longitude").val(ui.item.longitude);
+        var location = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
+        marker.setPosition(location);
+        map.setCenter(location);
       }
     });
   });
   
   //Add listener to marker for reverse geocoding
-  google.maps.event.addListener(departure, 'drag', function() {
+  google.maps.event.addListener(marker, 'drag', function() {
     geocoder.geocode({'latLng': marker.getPosition()}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         if (results[0]) {
-          $('.address').val(results[0].formatted_address);
+          $('#address1').val(results[0].formatted_address);
           $('#latitude').val(marker.getPosition().lat());
           $('#longitude').val(marker.getPosition().lng());
         }
       }
     });
   });
+<<<<<<< HEAD
 
   // Form posting event
   $('#form').submit(function() {
@@ -203,3 +266,6 @@ $(document).ready(function() {
   })
   
 });
+=======
+*/
+>>>>>>> Två markers i index
