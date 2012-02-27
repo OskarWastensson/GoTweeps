7b3e7818549376fa8523e51eb168fd73aaf4d49e
Marc Taillefer <marctai@gmail.com>
2012-02-27 13:59:21 +0100

    something

--- frontend/my_trips.js
+++ frontend/my_trips.js
@@ -1,31 +1,36 @@
 $(document).ready(function(){
 
 	$.getJSON('test.json', function(data){
+				
+		for (var i = 0; i < data.length; i++) {
+						
+			var passengers = data[i].passengers;
+			var pending = 0;
+			var confirmed = 0; 
+			for (var j = 0; j < passengers.length; j++) {
+				var confirmed_by_driver = passengers[j].confirmed_by_driver;
+				confirmed_by_driver == "1" ? confirmed++ : pending++;
+			}
 
-		
-		var tripEvent 	= data[0].tag,
-			tripDate 	= data[0].eta,
-			legs		= data.legs,
-			passengers 	= legs[legs.length-1].passengers,
-			confirmed 	= [],
-			unconfirmed = [];
-
-
-			for(i = 0; i < passengers.length; i++ ){
-				var confirms = passengers[i].confirmed_by_driver;
-
-				if(confirms == 0){
-					unconfirmed.push(confirms);
-				}else{
-					confirmed.push(confirms);
-				}
-			};
-
-		
-	
+			var tableData = {};
+			tableData.destination = data[i].tag;
+			tableData.eta = data[i].eta;
+			tableData.confirmed = confirmed;
+			tableData.pending = pending;
 			
-
+			var tr = createTableRow(tableData);  
+			$("#driverTable").append(tr);
+				
+		}
 	});
 
+	function createTableRow(tableData) {
+		var $tableRow = $("<tr/>");
+		$.each(tableData, function(key, value) {
+			$("<td/>").html(value)
+					  .appendTo($tableRow);
+		});
+		return $tableRow;
+	}
 
 });
