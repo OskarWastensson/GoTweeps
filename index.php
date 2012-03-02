6284a148ff84a989e68b9fcc53b05dc1d70f84b5
Hector Romo <hectitor.romo@gmail.com>
2012-03-02 13:13:58 +0100

    animation

--- index.php
+++ index.php
@@ -10,9 +10,11 @@
   <head>
     <meta charset="utf-8">
     <title>GoTweeps - Startsida</title>
+    <link rel="stylesheet" type="text/css" href="animation/style.css">
     <link type="text/css" rel="stylesheet" href="css/index.css" />
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"></script>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.js"></script>
+    <script type="text/javascript" src="animation/script.js"></script>
   </head>
   <body>
 
@@ -27,6 +29,31 @@
         <li><a href="index.php">Logga ut</a></li>
       </ul>
     </div>
+
+    <div id="container" class="item">
+
+      <div id="birdLeft" class="item">
+        <img src="animation/birdLeft.png">
+      </div>
+
+      <div id="birdRight" class="item">
+        <img id="bR" src="animation/birdRight.png" class="item">
+        <img id="wheels" src="wheels.png" class="item">
+      </div>
+
+      <div id="textWrapper" class="item">
+        <img id="text" src="animation/text2.png" class="item">
+      </div>
+
+      <div id="birdLeft2" class="item">
+        <img class="item" src="animation/birdLeft.png">
+      </div>
+
+      <div id="birdLeftYellow" class="item">
+        <img id="yellow" src="animation/yellow.png" class="item">
+      </div>
+    </div>
+    <embed src="animation/gotweeps.mp3" width="0" height="0" autoplay="true" loop="false"></embed>
     
   </body>
 </html>
